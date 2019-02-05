0cae483b4c1552f0bf922a0bf29c489443e49561
david-knigge <davidknigge@live.nl>
2019-02-06 00:10:25 +0100

    working potatohead

--- app/src/main/java/nl/uva/student/davidknigge/mrpotatohead/MainActivity.java
+++ app/src/main/java/nl/uva/student/davidknigge/mrpotatohead/MainActivity.java
@@ -2,6 +2,10 @@ package nl.uva.student.davidknigge.mrpotatohead;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void checkClicked(View v) {
+        Log.d("potato", "checkClicked: ");
+        CheckBox checkbox = (CheckBox) v;
+
+        String item = checkbox.getText().toString();
+
+        int rID = getResources().getIdentifier("imageView" + item, "id", getPackageName());
+        ImageView image = (ImageView) findViewById(rID);
+
+        if (checkbox.isChecked()) {
+            image.setVisibility(View.VISIBLE);
+        } else {
+            image.setVisibility(View.INVISIBLE);
+        }
     }
 }
