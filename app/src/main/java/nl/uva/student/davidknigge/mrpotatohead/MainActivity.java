7d7a0bd41b050c37b63e6e0e31c14cf379629651
david-knigge <davidknigge@live.nl>
2019-02-06 17:19:48 +0100

    landscape mode, randomize, reset button, memory management

--- app/src/main/java/nl/uva/student/davidknigge/mrpotatohead/MainActivity.java
+++ app/src/main/java/nl/uva/student/davidknigge/mrpotatohead/MainActivity.java
@@ -4,8 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,10 +16,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FrameLayout container = findViewById(R.id.imageContainer);
+
+        if (savedInstanceState != null) {
+            short[] visibilityArray = savedInstanceState.getShortArray("visibilityArray");
+
+            for (int i = 0; i < container.getChildCount(); i++) {
+                View child = container.getChildAt(i);
+                if (child instanceof ImageView) {
+                    ImageView image = (ImageView) child;
+                    image.setVisibility((int) visibilityArray[i]);
+                }
+            }
+        }
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        FrameLayout container = findViewById(R.id.imageContainer);
+
+        short[] visibilityArray = new short[container.getChildCount()];
+
+        for (int i = 0; i < container.getChildCount(); i++) {
+
+            View child = container.getChildAt(i);
+            if (child instanceof ImageView) {
+                ImageView image = (ImageView) child;
+                int visibility = image.getVisibility();
+                visibilityArray[i] = (short) visibility;
+            }
+        }
+        outState.putShortArray("visibilityArray", visibilityArray);
     }
 
     public void checkClicked(View v) {
-        Log.d("potato", "checkClicked: ");
         CheckBox checkbox = (CheckBox) v;
 
         String item = checkbox.getText().toString();
@@ -28,6 +65,36 @@ public class MainActivity extends AppCompatActivity {
             image.setVisibility(View.VISIBLE);
         } else {
             image.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    public void randomizeClicked(View v) {
+        cycleBoxes(v, true);
+    }
+
+    public void resetClicked(View v) {
+        cycleBoxes(v, false);
+    }
+
+    public void cycleBoxes(View v, boolean assignRandom) {
+        ViewGroup checkBoxContainer = (ViewGroup) v.getParent();
+        CheckBox checkbox = null;
+
+        for (int pos = 0; pos < checkBoxContainer.getChildCount(); pos++) {
+
+            View view = checkBoxContainer.getChildAt(pos);
+
+            if (view instanceof CheckBox) {
+                checkbox = (CheckBox) view;
+                checkbox.setChecked(false);
+
+                if (assignRandom) {
+                    if (Math.random() < 0.5) {
+                        checkbox.setChecked(true);
+                    }
+                }
+                checkClicked(view);
+            }
         }
     }
 }
