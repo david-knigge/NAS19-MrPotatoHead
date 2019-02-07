4268dc81b9c61cf9af0bcafa958a21ac74ef2b01
david knigge <davidknigge@live.nl>
2019-02-07 19:44:51 +0100

    landscape mode, randomize, reset button, memory management

--- app/src/main/java/nl/uva/student/davidknigge/mrpotatohead/MainActivity.java
+++ app/src/main/java/nl/uva/student/davidknigge/mrpotatohead/MainActivity.java
@@ -12,27 +12,37 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    /*
+     * Is called when this activity is loaded. Restores previously saved instance state by checking
+     * if Bundle is set and contains ImageView visibility values. Sets ImageView visibility values
+     * for all images in imageContainer.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FrameLayout container = findViewById(R.id.imageContainer);
-
         if (savedInstanceState != null) {
             short[] visibilityArray = savedInstanceState.getShortArray("visibilityArray");
 
-            for (int i = 0; i < container.getChildCount(); i++) {
-                View child = container.getChildAt(i);
-                if (child instanceof ImageView) {
-                    ImageView image = (ImageView) child;
-                    image.setVisibility((int) visibilityArray[i]);
+            if (visibilityArray != null) {
+                FrameLayout container = findViewById(R.id.imageContainer);
+
+                for (int i = 0; i < container.getChildCount(); i++) {
+                    View child = container.getChildAt(i);
+                    if (child instanceof ImageView) {
+                        ImageView image = (ImageView) child;
+                        image.setVisibility((int) visibilityArray[i]);
+                    }
                 }
             }
         }
-
     }
 
+    /*
+     * Is called when activity instance is destroyed. Creates array containing visiblity values for
+     * all ImageViews contained in the imageContainer layout.
+     */
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
@@ -53,13 +63,21 @@ public class MainActivity extends AppCompatActivity {
         outState.putShortArray("visibilityArray", visibilityArray);
     }
 
+    /*
+     * Called when any checkbox is clicked. Finds ImageView corresponding to this checkbox and sets
+     * visibilty value based on whether or not the checkbox is checked.
+     */
     public void checkClicked(View v) {
         CheckBox checkbox = (CheckBox) v;
 
         String item = checkbox.getText().toString();
 
-        int rID = getResources().getIdentifier("imageView" + item, "id", getPackageName());
-        ImageView image = (ImageView) findViewById(rID);
+        int rID = getResources().getIdentifier(
+                "imageView" + item,
+                "id",
+                getPackageName()
+        );
+        ImageView image = findViewById(rID);
 
         if (checkbox.isChecked()) {
             image.setVisibility(View.VISIBLE);
@@ -68,32 +86,42 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /*
+     * Called when randomize button is clicked. Calls setBoxes to randomly assign values to the
+     * checkboxes.
+     */
     public void randomizeClicked(View v) {
-        cycleBoxes(v, true);
+        setBoxes(v, true);
     }
 
+    /*
+     * Called when reset button is clicked. Calls setBoxes to clear all checkboxes.
+     */
     public void resetClicked(View v) {
-        cycleBoxes(v, false);
+        setBoxes(v, false);
     }
 
-    public void cycleBoxes(View v, boolean assignRandom) {
+    /*
+     * Loops over all child Views in the checkbox container. Resets all checkboxes to unchecked, or
+     * randomly assigns a value based on assignRandom. Updates ImageView visibility corresponding to
+     * every checkbox.
+     */
+    public void setBoxes(View v, boolean assignRandom) {
         ViewGroup checkBoxContainer = (ViewGroup) v.getParent();
-        CheckBox checkbox = null;
 
         for (int pos = 0; pos < checkBoxContainer.getChildCount(); pos++) {
 
             View view = checkBoxContainer.getChildAt(pos);
 
             if (view instanceof CheckBox) {
-                checkbox = (CheckBox) view;
-                checkbox.setChecked(false);
+                CheckBox checkbox = (CheckBox) view;
 
-                if (assignRandom) {
-                    if (Math.random() < 0.5) {
-                        checkbox.setChecked(true);
-                    }
+                if (assignRandom && Math.random() < 0.5) {
+                    checkbox.setChecked(true);
+                } else {
+                    checkbox.setChecked(false);
                 }
-                checkClicked(view);
+                checkClicked(checkbox);
             }
         }
     }
